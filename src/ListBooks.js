606b32466ad703499efa9477ae494908ef23137f
PhilipDeFraties <philipjfry338@gmail.com>
2023-03-23 19:48:15 -0600

    feat: add ListBooks component add new component that takes array of book objects upon intialization books are sorted by shelf value into object

---
+++ src/ListBooks.js
@@ -0,0 +1,32 @@
+const ListBooks = ({books}) => {
+    const sortBooks = (books) => {
+        let sortedBooks = {};
+        books.forEach((book) => {
+            if (booksObj[book.shelf]) {
+                booksObj[book.shelf].push(book)
+             } else {
+                booksObj[book.shelf] = [book]
+             };
+        });
+        return sortedBooks;
+    };
+
+    const booksByStatus = sortBooks(books);
+
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                </div>
+            </div>
+            <div className="open-search">
+                <a>Add a book</a>
+            </div>
+        </div>
+    );
+};
+
+export default ListBooks;
